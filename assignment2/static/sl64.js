56419070e73fc59dd666a4edfa35ebf2d8802db4
Rahul Hari <52792591+rahulharikumarr@users.noreply.github.com>
2024-02-14 19:39:33 -0800

    finished company tab

--- assignment2/static/sl64.js
+++ assignment2/static/sl64.js
@@ -22,6 +22,14 @@ document.addEventListener('DOMContentLoaded', function () {
                         displayErrorMessage(`Error: No record has been found. Please enter a valid stock symbol`);
                     } else {
                         console.log(data);
+                        var img = data.company_data.logo
+                        var name = data.company_data.name
+                        var ticker = data.company_data.ticker
+                        var exchange = data.company_data.exchange
+                        var start = data.company_data.ipo
+                        var category = data.company_data.finnhubIndustry
+                        generateCompanyTab(img, name, ticker, exchange, start, category)
+
                         nav.style.display = 'flex';
                         if(document.getElementById('error-message'))
                         {
@@ -66,6 +74,81 @@ document.addEventListener('DOMContentLoaded', function () {
             errorDiv.textContent = errorMessage;
         }
     }
+
+    function generateCompanyTab(img, name, ticker, exchange, start, category) {
+        // Check if the error div already exists, if not, create and append it
+
+        var existingCompanyParent = document.getElementById('company_parent');
+        if (existingCompanyParent) {
+        existingCompanyParent.remove();
+        }
+        if (!document.getElementById('error-message')) {
+
+            // Create a parent div for the company details
+            var company_parent = document.createElement('div');
+            company_parent.id = 'company_parent';
+            company_parent.classList.add('company-details');  // Add the common class
+            var tab_containers = document.querySelector('.tab-containers')
+            tab_containers.appendChild(company_parent);
+    
+            // Create an image element
+            var img_div = document.createElement('div')
+            img_div.id = 'company_element_container'
+            var imgElement = document.createElement('img');
+            company_parent.appendChild(img_div)
+            imgElement.src = img;
+            imgElement.alt = 'Company Logo';
+            imgElement.style.width = '150px';
+            imgElement.style.height = '150px';
+            img_div.appendChild(imgElement)
+    
+            // name
+            var name_div = document.createElement('div')
+            name_div.id = 'company_element_container'
+            var name_paragraph = document.createElement('p');
+            name_paragraph.textContent = `Company Name: ${name}`;
+            name_paragraph.style.fontWeight = 'bold';
+            company_parent.appendChild(name_div);
+            name_div.appendChild(name_paragraph)
+
+            //ticker
+            var ticker_div = document.createElement('div')
+            ticker_div.id = 'company_element_container'
+            var ticker_paragraph = document.createElement('p');
+            ticker_paragraph.textContent = `Stock Ticker Symbol: ${ticker}`;
+            ticker_paragraph.style.fontWeight = 'bold';
+            company_parent.appendChild(ticker_div)
+            ticker_div.appendChild(ticker_paragraph)
+    
+            //exchange
+            var exchange_div = document.createElement('div')
+            exchange_div.id = 'company_element_container'
+            var exchange_paragraph = document.createElement('p');
+            exchange_paragraph.textContent = `Stock Exchange Code: ${exchange}`;
+            exchange_paragraph.style.fontWeight = 'bold';
+            company_parent.appendChild(exchange_div);
+            exchange_div.appendChild(exchange_paragraph)
+    
+            //start data
+            var start_div = document.createElement('div')
+            start_div.id = 'company_element_container'
+            var start_paragraph = document.createElement('p');
+            start_paragraph.textContent = `Company Start Date: ${start}`;
+            start_paragraph.style.fontWeight = 'bold';
+            company_parent.appendChild(start_div);
+            start_div.appendChild(start_paragraph)
+    
+            //category
+            var category_div = document.createElement('div')
+            category_div.id = 'company_element_container'
+            var category_paragraph = document.createElement('p');
+            category_paragraph.textContent = `Category: ${category}`;
+            category_paragraph.style.fontWeight = 'bold';
+            company_parent.appendChild(category_div);
+            category_div.appendChild(category_paragraph)
+        }
+    }
+    
 
 var searchButton = document.getElementById('search-button');
 if (searchButton) {
